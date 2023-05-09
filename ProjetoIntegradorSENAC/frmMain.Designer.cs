c4f68d8de54f006bfe2bb3aecad7e62e883485f2
Leonardo <leo11oliveirasantos@hotmail.com>
2023-05-08 22:23:08 -0300

    add left panel icons

--- ProjetoIntegradorSENAC/frmMain.Designer.cs
+++ ProjetoIntegradorSENAC/frmMain.Designer.cs
@@ -28,12 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.pn_Left = new System.Windows.Forms.Panel();
             this.pn_Main = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pn_Left.SuspendLayout();
             this.SuspendLayout();
             // 
             // pn_Left
             // 
+            this.pn_Left.Controls.Add(this.button4);
+            this.pn_Left.Controls.Add(this.button3);
+            this.pn_Left.Controls.Add(this.button2);
+            this.pn_Left.Controls.Add(this.button1);
             this.pn_Left.Dock = System.Windows.Forms.DockStyle.Left;
             this.pn_Left.Location = new System.Drawing.Point(0, 0);
             this.pn_Left.Name = "pn_Left";
@@ -48,6 +60,77 @@
             this.pn_Main.Size = new System.Drawing.Size(950, 616);
             this.pn_Main.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ImageKey = "Home.png";
+            this.button1.ImageList = this.imageList1;
+            this.button1.Location = new System.Drawing.Point(0, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 67);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ImageKey = "Truck.png";
+            this.button2.ImageList = this.imageList1;
+            this.button2.Location = new System.Drawing.Point(0, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 67);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.IndianRed;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(0, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 67);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ImageKey = "Profile.png";
+            this.button4.ImageList = this.imageList1;
+            this.button4.Location = new System.Drawing.Point(0, 528);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 67);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Home.png");
+            this.imageList1.Images.SetKeyName(1, "HomeSelect.png");
+            this.imageList1.Images.SetKeyName(2, "Profile.png");
+            this.imageList1.Images.SetKeyName(3, "ProfileSelect.png");
+            this.imageList1.Images.SetKeyName(4, "Truck.png");
+            this.imageList1.Images.SetKeyName(5, "TruckSelect.png");
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -58,6 +141,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmMain";
             this.Text = "frmMain";
+            this.pn_Left.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,5 +150,10 @@
 
         private Panel pn_Left;
         private Panel pn_Main;
+        private Button button4;
+        private ImageList imageList1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
     }
 }
